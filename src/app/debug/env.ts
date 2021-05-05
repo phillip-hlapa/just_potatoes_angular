cd4db1a9cc1f849cf92e94a56c47078cc9a735be
phillip-hlapa <philliphlapa2015@gmail.com>
2021-05-05 15:54:54 +0200

    final

--- src/app/debug/env.ts
+++ src/app/debug/env.ts
@@ -1,5 +1,5 @@
 export const environment = {
     //prod_url: 'http://localhost:1993',
-    prod_url_heroku: 'https://rocky-brook-00154.herokuapp.com',
-    prod_url_glitch: 'https://quasar-holistic-bicycle.glitch.me'
+    prod_url_heroku: 'http://localhost:1993',
+    prod_url_glitch: 'http://localhost:1993'
 };
