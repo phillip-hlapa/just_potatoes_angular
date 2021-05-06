985b795b28c662a4066837ad5f8a26fcda1b389d
phillip-hlapa <philliphlapa2015@gmail.com>
2021-05-06 14:28:29 +0200

    finally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,6 @@ import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
@@ -37,10 +29,11 @@ import {MatButtonModule} from "@angular/material/button";
 import { DebugComponent } from './debug/debug.component';
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { CdkTableModule} from '@angular/cdk/table';
-
+import { SpinnerCircularModule } from 'spinners-angular/spinner-circular';
 @NgModule({
     imports: [
 
+        SpinnerCircularModule,
         CdkTableModule,
         MatPaginatorModule,
         BrowserAnimationsModule,
