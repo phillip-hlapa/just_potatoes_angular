6815f531266f33794764181994a35e7c073e58a4
Your Name <you@example.com>
2021-08-10 18:29:55 +0200

    fix

--- src/app/debug/env.ts
+++ src/app/debug/env.ts
@@ -1,6 +1,7 @@
 export const environment = {
     //prod_url: 'http://localhost:1993',
-    prod_url_heroku: 'https://rocky-brook-00154.herokuapp.com',
+//     prod_url_heroku: 'https://rocky-brook-00154.herokuapp.com',
+    prod_url_heroku: 'https://desolate-shore-72903.herokuapp.com',
     prod_url_glitch: 'https://quasar-holistic-bicycle.glitch.me',
     //prod_url_heroku: 'http://localhost:1993',
     //prod_url_glitch: 'http://localhost:1993'
